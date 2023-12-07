d02092ae80454a9806d50b44d5b085cf38920fed
Revito <revitomanik@gmail.com>
2023-12-08 01:42:33 +0800

    Annyeong

---
+++ gwaenchana.c
@@ -0,0 +1,49 @@
+/*
+
+JUDUL : 
+
+DEKLARASI :
+
+ALGORITMA :
+
+
+*/
+
+
+//Kumpulan Library
+#include <stdio.h>
+#include <time.h>
+
+
+//Prosedur
+void cls() {
+    system("cls");
+}
+
+void greeting() {
+    printf("Halo! Selamat datang di (...). \n");
+}
+
+void thanks() {
+    printf("Terima kasih telah (...). Sampai jumpa!");
+}
+
+
+//Variabel Global
+//...
+
+
+//Fungsi Hitung
+//...
+
+
+//Fungsi Main
+int main() {
+
+
+    return 0;
+}
+
+
+//Billing to txt
+//...
