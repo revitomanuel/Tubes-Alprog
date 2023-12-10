5efa8f8711992a266e1a56fa4f78e89a1851e5cc
Revito <revitomanik@gmail.com>
2023-12-10 17:14:23 +0800

    Update dessert

--- gwaenchana.c
+++ gwaenchana.c
@@ -21,10 +21,10 @@ void cls() {
 }
 
 void greeting() {
-    printf("================================= \n");
-    printf("        SELAMAT DATANG DI         \n");
-    printf("      GWAENCHANA RESTAURANT       \n");
-    printf("================================= \n");
+    printf("=================================\n");
+    printf("        SELAMAT DATANG DI        \n");
+    printf("      GWAENCHANA RESTAURANT      \n");
+    printf("=================================\n\n");
 }
 
 void thanks() {
@@ -34,6 +34,8 @@ void thanks() {
 
 //Variabel Global
 int option_K, option_MA, option_MI, option_D;
+int jumlah;
+float ppn;
 char choice;
 
 //Fungsi Hitung
@@ -48,13 +50,13 @@ int main() {
 
     greeting();
 
-    printf("======================= \n");
-    printf(" MAU PESAN APA? \n");
-    printf("======================= \n");
-    printf("1. MAKANAN \n");
-    printf("2. MINUMAN \n");
-    printf("3. DESSERT \n");
-    printf("======================= \n");
+    printf("+====================+\n");
+    printf("|   MAU PESAN APA?   |\n");
+    printf("+===+================+\n");
+    printf("| 1 | MAKANAN        |\n");
+    printf("| 2 | MINUMAN        |\n");
+    printf("| 3 | DESSERT        |\n");
+    printf("+===+================+\n");
     printf("Silahkan masukkan pilihan Anda : ");
     scanf("%d", &option_K);
     printf("\n");
@@ -69,6 +71,10 @@ int main() {
             printf("    \n");
             printf("    \n");
             printf("    \n");
+            printf("Silahkan masukkan pilihan Anda : ");
+            scanf("%d", &option_D);
+            printf("Masukkan jumlah : ");
+            scanf("%d", &jumlah);
         }
         else if (option_K == 2) {
             printf("==================================== \n");
@@ -80,20 +86,27 @@ int main() {
             printf("    \n");
             printf("    \n");
             printf("    \n");
-        }
-        else if (option_K == 3) {
-            printf("=============================================== \n");
-            printf("            M E N U  D E S S E R T              \n");
-            printf("=============================================== \n");
-            printf(" NO | DESSERT                | HARGA           |\n");
-            printf("----------------------------------------------- \n");
-            printf("  1 | Bungeoppang            | Rp10.000        |\n");
-            printf("  2 |                                           \n");
-            printf("4.  \n");
-            printf("5.  \n");
-            printf("=============================================== \n");
             printf("Silahkan masukkan pilihan Anda : ");
             scanf("%d", &option_D);
+            printf("Masukkan jumlah : ");
+            scanf("%d", &jumlah);
+        }
+        else if (option_K == 3) {
+            printf("+========================================+\n");
+            printf("|         M E N U  D E S S E R T         |\n");
+            printf("+====+=================+=================+\n");
+            printf("| NO |     DESSERT     |      HARGA      |\n");
+            printf("+----+-----------------+-----------------+\n");
+            printf("| 1  | Bungeoppang     | Rp        10.000|\n");
+            printf("| 2  | Patbingsoo      | Rp        25.ooo|\n");
+            printf("| 3  | Hotteok         | Rp         8.000|\n");
+            printf("| 4  | Hwachae         | Rp        30.000|\n");
+            printf("| 5  | Jeonggwa        | Rp        15.000|\n");
+            printf("+====+=================+=================+\n");
+            printf("Silahkan masukkan pilihan Anda : ");
+            scanf("%d", &option_D);
+            printf("Masukkan jumlah : ");
+            scanf("%d", &jumlah);
         }
         else {
             printf("Mohon maaf, input yang Anda masukkan tidak cocok dengan opsi yang tersedia.\n\n");
